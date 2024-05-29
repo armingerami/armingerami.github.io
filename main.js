434551743db09a4b59049dbe5643f7c048eed7f3
armingerami <74568028+armingerami@users.noreply.github.com>
2024-05-29 12:01:08 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -4,8 +4,7 @@
 // server.listen(port);
 
 // import './style.css';
-import * as THREE from 'three';
-// import "http";
+import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js';
 // import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 // Setup
